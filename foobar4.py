1a93a2b1c822939f86ed485416bde670fc574c61
joshidhawal <joshidhawal2046@gmail.com>
2020-05-13 08:38:59 +0530

    updated foobar4 for long int

--- foobar4.py
+++ foobar4.py
@@ -2,26 +2,29 @@ def solution(x, y):
     # Your code here
     returnString=''
     
-    requiredm=int(x)
-    requiredf=int(y)
+    requiredm=long(x)
+    requiredf=long(y)
     
     m=requiredm
     f=requiredf
-    generation=0
+    generation=long(0)
     
     while(m>1 or f>1):
-        greatorNumber = 0
+        if(m==1 and f>m):
+            generation=generation+(f-m)
+            break
+        elif(m>f and f==1):
+            generation=generation+(m-f)
+            break
         if (m>f):
-            greatorNumber=m
-            m=greatorNumber-f
+            m=m-f
         else:
-            greatorNumber=f
-            f=greatorNumber-m
+            f=f-m
         generation+=1
+        #print 'm:',m,'f:',f
         if(m<1 or f<1):
             generation=0
             break
-    
     if generation==0:
         returnString='impossible'
     else:
@@ -29,7 +32,7 @@ def solution(x, y):
     
     return returnString
 
-x=4
-y=7
+x='100000000000000000000000000000000000000000000000000000000'
+y='999999999999'
 result = solution(x,y)
 print result
