70208225e9c59889496f76d839d3a17efcae8cff
joshidhawal <joshidhawal2046@gmail.com>
2020-05-21 10:02:52 +0530

    working on prepare the bunnies escape solution

--- foobar6.py
+++ foobar6.py
@@ -1,8 +1,13 @@
 def solution(map):
-    
-    print 'The length of the matrix is'
+    widthOfTheMatrix = len(map[0])
+    heightOfTheMatrix = len(map)
+    print 'The width of the matrix is', widthOfTheMatrix
+    print 'The width of the matrix is', heightOfTheMatrix
+
+    def findPath():
+        pass
 
 
-map = [[0, 1, 1, 0][0, 0, 0, 1][1, 1, 0, 0][1, 1, 1, 0]]
+map = [[0, 1, 1, 0],[0, 0, 0, 1],[1, 1, 0, 0],[1, 1, 1, 0]]
 availablePath = solution(map)
 print 'Available Path :',availablePath
