43b52373e38d967c98eb7229402c26682308394d
joshidhawal <joshidhawal2046@gmail.com>
2020-05-21 10:15:46 +0530

    'hi'

--- autopushcommit.py
+++ autopushcommit.py
@@ -2,8 +2,8 @@ import os
 
 commitMessage = input('Enter the Commit Message without quotes:\n')
 
-command = 'git commit -m \''+commitMessage+'\''
-
+command = 'git commit -m \''+str(commitMessage)+'\''
+print (command)
 os.system('git add .')
 os.system(command)
 os.system('git push origin')
